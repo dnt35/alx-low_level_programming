b757e78ff2d7ae6e7bb385509f3aa9d28da2f407
charles josiah <charlesikhalea4real@gmail.com>
2022-10-13 16:15:01 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,14 +7,13 @@
  *   *
  *
  *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
 {
-
+	
 	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-
+	  
+        return (0);
 }
