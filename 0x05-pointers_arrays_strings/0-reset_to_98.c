6d0c159050c5191f7a7c4d0ae9cc77a331e04e18
dnt35 <charlesikhalea4real@gmail.com>
2022-10-25 20:01:34 +0100

    blast

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * resets_to_98 point an int as parameter
+ * and update the vallue to 98
+ * @n: input integer.
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
