30dc4f36dd02daf207a717ef7f34b244ef3665ff
dnt35 <charlesikhalea4real@gmail.com>
2022-10-25 20:36:58 +0100

    tree

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,40 +1,16 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
+ * _strlen - returns the length of a string
+ * @s: input string
  *
- * Return: A integer
+ * Return: length of s
  */
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i = 0;
 
-	while (s[c])
-	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
-		c++;
-	}
-
-	ni *= min;
-	return (ni);
+	while (*(s + i))
+		i++;
+	return (i);
 }
