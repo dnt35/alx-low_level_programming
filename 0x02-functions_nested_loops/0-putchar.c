bf2dd7585adfb48429f0e01912179819926b778e
dnt35 <charlesikhalea4real@gmail.com>
2022-12-14 17:06:01 +0100

    we back

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,1 @@
+#include <holberton.h>																																				int main(void)											{																										char *sh = "Holberton";																							while (*sh)												{													_putchar(*sh);												sh++;																								}													_putchar("\n");																							return 0;																									}
